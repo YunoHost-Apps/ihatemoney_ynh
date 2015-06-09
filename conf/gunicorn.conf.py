96b92b6ee6e2986203e9a969ec4fe772ca234139
Jocelyn Delande <jocelyn@crapouillou.net>
2015-06-10 00:12:55 +0200

    socket is now in /tmp/ as /var/run/ihatemoney gets rm on each reboot fixes #3

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -4,4 +4,4 @@ debug = True
 workers = 3
 logfile = "/var/log/ihatemoney/budget.gunicorn.log"
 loglevel = "info"
-bind = "unix:/var/run/ihatemoney/budget.gunicorn.sock"
+bind = "unix:/tmp/budget.gunicorn.sock"
